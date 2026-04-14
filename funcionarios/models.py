37b78f7f7fc22f1033a291f11efd5e7da9c0c927
Edoardo henrique <2006edoardo@gmail.com>
2026-04-14 15:13:56 -0300

    Modelos: Product, Order e Profile

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 #classe Mãe:
 class Base(models.Model):
@@ -15,4 +16,25 @@ class employee(Base):
     def __str__(self):
         return self.name
     
+class Product(Base):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.name
+
+class Order(Base):
+    client_name = models.CharField(max_length=100)
+    products = models.ManyToManyField(Product, related_name='orders')
+    
+    def __str__(self):
+        return f"Order {self.id} - {self.client_name}"
+
+class Profile(Base):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cpf = models.CharField(max_length=14, unique=True)
+    photo = models.ImageField(upload_to='profiles/', null=True, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
