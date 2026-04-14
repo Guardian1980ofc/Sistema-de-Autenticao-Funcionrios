976d64bf69ccd562907d1ea1e2d5a3a330a7d1a4
Edoardo henrique <2006edoardo@gmail.com>
2026-04-14 12:55:13 -0300

    Modelos: Herança de classe abstrata com auditoria e soft delete

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+#classe Mãe:
+class Base(models.Model):
+    created_at = models.DateTimeField("Creation", auto_now_add=True)
+    updated_at = models.DateTimeField("Update", auto_now=True)
+    is_deleted = models.BooleanField("Deleted", default=False) #o soft delete para o abstado apagar sem querer
+
+    class Meta:
+        abstract = True #para fazer o Djago não criar uma tabela dele
+
+class employee(Base):
+    name = models.CharField("Name", max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
